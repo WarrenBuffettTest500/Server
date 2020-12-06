6530446ca4bb4aedad6eeeee96a255307b44c8b4
YOONA LEE <lya1290@gmail.com>
2020-12-07 06:37:08 +0900

    feat: change about sotck Detail API

--- services/stockDetail.service.js
+++ services/stockDetail.service.js
@@ -1,7 +1,7 @@
 const { CompanyProfile } = require('../models');
 const { wrapperRequest } = require('../utils/wrapperRequest');
 
-exports.findbyKeyWord = async keyWord => {
+exports.findbyKeyWord = async (keyWord, date) => {
   try {
     const hasTicker = await CompanyProfile.findOne({
       where: { symbol: keyWord }
@@ -11,15 +11,15 @@ exports.findbyKeyWord = async keyWord => {
 
     const options = {
       method: 'GET',
-      url: 'https://alpha-vantage.p.rapidapi.com/query',
-      qs: {function: 'GLOBAL_QUOTE', symbol: keyWord},
+      url: 'https://twelve-data1.p.rapidapi.com/time_series',
+      qs: {symbol: keyWord, interval: date || '1day', outputsize: '30', format: 'json'},
       headers: {
         'x-rapidapi-key': process.env.X_RAPIDAPI_KEY,
-        'x-rapidapi-host': 'alpha-vantage.p.rapidapi.com',
+        'x-rapidapi-host': 'twelve-data1.p.rapidapi.com',
         useQueryString: true
       }
     };
-
+    
     const stockDetail = await wrapperRequest(options);
 
     if (!stockDetail) return;
