f96c5375a3ca2e8a0d35b7397cefbf3624ce186b
YOONA LEE <lya1290@gmail.com>
2020-12-06 10:35:24 +0900

    feat: add CompanyProfile model

--- routes/tickers.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/:keyword', (req, res, next) => {
-  console.log(req.params, 'req.params');
-});
-
-module.exports = router;
