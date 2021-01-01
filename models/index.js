4f350bd81b58bf09835daf7424f93f171d909e7b
Jaehoo Im <im_jaehoo@kakao.com>
2021-01-02 05:01:14 +0900

    first deploy

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(path.join(__dirname, '..', 'config', 'config.js'))[env];
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(config);
 
 db.User = require('./User')(sequelize, Sequelize);
 db.PreferenceInfo = require('./PreferenceInfo')(sequelize, Sequelize);
