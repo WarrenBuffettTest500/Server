984f202a482a78ff3dc1887712e9ddaa1f92973a
Jaehoo Im <im_jaehoo@kakao.com>
2021-01-08 01:20:17 +0900

    refactor: change route underscores to hyphens

--- routes/users.js
+++ routes/users.js
@@ -7,16 +7,16 @@ const { getPortfolio, getPortfolioRecommendationsByPreference, getPortfolioRecom
 const verifyToken = require('../middlewares/verifyToken');
 const PATHS = require('../constants/paths');
 
-router.get('/current_user', verifyToken, getCurrentUser);
+router.get('/current-user', verifyToken, getCurrentUser);
 router.put('/:user_id', verifyToken, updateUserInfo);
-router.get('/:user_id/preference_infos/:preference_info_id', verifyToken, getPreferenceInfo);
-router.post('/:user_id/preference_infos', verifyToken, createPreferenceInfo);
-router.put('/:user_id/preference_infos', verifyToken, updatePreferenceInfo);
+router.get('/:user_id/preference-infos/:preference_info_id', verifyToken, getPreferenceInfo);
+router.post('/:user_id/preference-infos', verifyToken, createPreferenceInfo);
+router.put('/:user_id/preference-infos', verifyToken, updatePreferenceInfo);
 
 router.get('/:user_id/portfolio', verifyToken, getPortfolio);
-router.post('/:user_id/portfolio_items', verifyToken, createPortfolioItem);
-router.put('/:user_id/portfolio_items/:portfolio_item_id', verifyToken, updatePortfolioItem);
-router.delete('/:user_id/portfolio_items/:portfolio_item_id', verifyToken, deletePortfolioItem);
+router.post('/:user_id/portfolio-items', verifyToken, createPortfolioItem);
+router.put('/:user_id/portfolio-items/:portfolio_item_id', verifyToken, updatePortfolioItem);
+router.delete('/:user_id/portfolio-items/:portfolio_item_id', verifyToken, deletePortfolioItem);
 
 router.get('/:user_id/portfolios/recommendations/preference', verifyToken, getPortfolioRecommendationsByPreference);
 router.get('/:user_id/portfolios/recommendations/portfolio', verifyToken, getPortfolioRecommendationsByPortfolio);
